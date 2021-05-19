4983395643d13fef4a8247df6385f333c53c33a0
Timothy Coleman <timhc22@gmail.com>
2021-05-19 18:13:10 +0100

    Add categories

--- src/App.tsx
+++ src/App.tsx
@@ -22,34 +22,66 @@ function App() : JSX.Element {
         </Toolbar>
       </AppBar>
       <Container className="site-container" maxWidth="sm">
-        <Typography>The definitive list for projects in the Zero Knowledge Space (currently in no particular order)</Typography>
-        <Typography class="red">This site is in Beta, please <a target="_blank" href="https://unegma.com/contact">drop me a message</a> if you feel there is any error</Typography>
+
+        <Typography>The definitive list for projects in the Zero Knowledge Space</Typography>
+        <Typography className="red">This site is in Beta, please <a target="_blank" href="https://unegma.com/contact">drop a message</a> if you feel there is any error</Typography>
+
+        <br/>
+        <Divider/>
+        <br/>
+
+        <Typography variant="h5">Community</Typography>
+        <p className="bold"><a href="https://www.zeroknowledge.fm/" target="_blank" className="hi">Zero Knowledge Podcast</a> - Where we talk about zero knowledge research and the decentralised web</p>
+        <p className="bold"><a href="https://zkproof.org/" target="_blank" className="hi">ZKProof Standards</a> - A global movement to standardize and mainstream advanced cryptography by building a community-driven trust ecosystem</p>
+
+        <br/>
+        <Divider/>
+        <br/>
+
+        <Typography variant="h5">Privacy Tools</Typography>
+        <p><a href="https://www.hyperledger.org/use/ursa" target="_blank" className="hi">Hyperledger Ursa</a> - Hyperledger Ursa is a shared cryptographic library, it enables implementations to avoid duplicating other cryptographic work and hopefully increase security in the process</p>
+        <p><a href="https://github.com/EYBlockchain/nightfall" target="_blank" className="hi">Nightfall</a> - EY Nightfall</p>
+        <p><a href="https://github.com/EYBlockchain/starlight" target="_blank" className="hi">Starlight</a> - Generate a zApp from a Solidity contract</p>
+        <p><a href="https://github.com/haael/white-box-fapkc" target="_blank" className="hi">White-box-fapkc</a> - White-Box Cryptography based on FAPKC algorithm</p>
+        <p><a href="https://zokrates.github.io/" target="_blank" className="hi">ZoKrates</a> - ZoKrates is a toolbox for zkSNARKs on Ethereum</p>
 
         <br/>
 
-        <Divider/>
-        <p className="bold"><a href="https://www.zeroknowledge.fm/" target="_blank" className="hi">Zero Knowledge Podcast</a> - Where we talk about zero knowledge research and the decentralised web</p>
-        <p className="bold"><a href="https://zkproof.org/" target="_blank" className="hi">ZKProof Standards</a> - A global movement to standardize and mainstream advanced cryptography by building a community-driven trust ecosystem</p>
-        <Divider/>
-
-        <p><a href="https://aztec.network/" target="_blank" className="hi">Aztec</a> - Scalable Privacy on Ethereum</p>
-        <p><a href="https://zksync.io/" target="_blank" className="hi">ZK Sync</a> - Rely on math, not validators</p>
-        <p><a href="https://www.fluidex.io/" target="_blank" className="hi">Fluidex</a> - zk-rollup orderbook DEX on Ethereum using PLONK</p>
-        <p><a href="https://tornado.cash/" target="_blank" className="hi">Tornado Cash</a> - A fully decentralized protocol for private transactions on Ethereum</p>
-        <p><a href="https://github.com/EYBlockchain/nightfall" target="_blank" className="hi">Nightfall</a> - EY Nightfall</p>
-        <p><a href="https://zokrates.github.io/" target="_blank" className="hi">ZoKrates</a> - ZoKrates is a toolbox for zkSNARKs on Ethereum</p>
+        <Typography variant="h5">Consultancy</Typography>
         <p><a href="http://extropy.foundation/" target="_blank" className="hi">Extropy Foundation</a> - Blockchain Consultancy</p>
         <p><a href="http://extropy.io/" target="_blank" className="hi">Extropy.io</a> - Making blockchain knowledge accessible</p>
-        <p><a href="https://www.horizen.io/" target="_blank" className="hi">Horizen.io</a> - Unlock the power of blockchain for real-world use cases</p>
-        <p><a href="https://vitalik.ca/" target="_blank" className="hi">Vitalik Buterin's website</a> - Plenty of ZK articles on here</p>
-        <p><a href="https://www.hyperledger.org/use/ursa" target="_blank" className="hi">Hyperledger Ursa</a> - Hyperledger Ursa is a shared cryptographic library, it enables implementations to avoid duplicating other cryptographic work and hopefully increase security in the process</p>
-        <p><a href="https://github.com/haael/white-box-fapkc" target="_blank" className="hi">White-box-fapkc</a> - White-Box Cryptography based on FAPKC algorithm</p>
+
+        <br/>
+
+        <Typography variant="h5">Info/Research</Typography>
         <p><a href="https://matter-labs.io/" target="_blank" className="hi">Matter Labs</a> - We are an engineering team passionate about liberty, blockchain, and math</p>
-        <p><a href="https://hermez.io/" target="_blank" className="hi">Hermez</a> - Scalable payments. Decentralised by design, open for everyone</p>
-        <p><a href="https://github.com/EYBlockchain/starlight" target="_blank" className="hi">Starlight</a> - Generate a zApp from a Solidity contract</p>
-        <p><a href="https://minaprotocol.com/" target="_blank" className="hi">Mina Protocol</a> - The world's lightest blockchain, powered by participants</p>
-        <p><a href="https://zks.org/" target="_blank" className="hi">ZKSwap</a> - ZK-Rollups based layer-2 DEX with the AMM model</p>
+        <p><a href="https://vitalik.ca/" target="_blank" className="hi">Vitalik Buterin's website</a> - Articles and info by Vitalik Buterin</p>
         <p><a href="https://zkvalidator.com/" target="_blank" className="hi">ZKValidator</a> - Stake & Support ZKP Research</p>
+
+        <br/>
+
+        <Typography variant="h5">Layer 2</Typography>
+        <p><a href="https://aztec.network/" target="_blank" className="hi">Aztec</a> - Scalable Privacy on Ethereum</p>
+        <p><a href="https://zksync.io/" target="_blank" className="hi">ZK Sync</a> - Rely on math, not validators</p>
+
+        <br/>
+
+        <Typography variant="h5">DEX/DApps</Typography>
+        <p><a href="https://www.fluidex.io/" target="_blank" className="hi">Fluidex</a> - zk-rollup orderbook DEX on Ethereum using PLONK</p>
+        <p><a href="https://hermez.io/" target="_blank" className="hi">Hermez</a> - Scalable payments. Decentralised by design, open for everyone</p>
+        <p><a href="https://tornado.cash/" target="_blank" className="hi">Tornado Cash</a> - A fully decentralized protocol for private transactions on Ethereum</p>
+        <p><a href="https://zks.org/" target="_blank" className="hi">ZKSwap</a> - ZK-Rollups based layer-2 DEX with the AMM model</p>
+
+        <br/>
+
+        <Typography variant="h5">Blockchain</Typography>
+        <p><a href="https://minaprotocol.com/" target="_blank" className="hi">Mina Protocol</a> - The world's lightest blockchain, powered by participants</p>
+        <p><a href="https://www.horizen.io/" target="_blank" className="hi">Horizen.io</a> - Unlock the power of blockchain for real-world use cases</p>
+
+        <br/>
+
+
+        <Typography variant="h5">Various</Typography>
         <p><a href="https://celo.org/" target="_blank" className="hi">Celo</a> - Global payments infrastructure built for mobile</p>
 
         <br/>
